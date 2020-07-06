baa2437bf510dcf6a4590b42dd54267a2b13f618
as324 <as324@hdm-stuttgart.de>
2020-07-06 17:49:34 +0200

    added Caldav getNextMeeting function

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler
+from datetime import datetime, timedelta, time, date
+import caldav
+from caldav.elements import dav, cdav
+from dateutil import tz
 
 
 class MyNextMeeting(MycroftSkill):
@@ -27,11 +31,26 @@ class MyNextMeeting(MycroftSkill):
         self.log.info("caldav:",self.caldav)
         self.log.info("username:",self.userName)
         self.log.info("password:",self.password)
+        self.client = caldav.DAVClient(self.caldav)
+        self.principal = self.client.principal()
+        self.calendars = self.principal.calendars()
+        self.calendar = self.calendars[0]
 
     def get_next_appointment_info(self):
-        apmnt_Date = "June 22, 2020"
-        apmnt_Time = "4 pm"
-        apmnt_Title = "Speech Interaction class"
+        now = datetime.now()
+        end = now + timedelta(1)
+        results = self.calendar.date_search(now, end)
+        list = []
+        for event in results:
+            start = event.instance.vevent.dtstart.value
+            end = event.instance.vevent.dtend.value
+            summary = event.instance.vevent.summary.value
+            list.append([start, end, summary])
+        list.sort()
+        event = list[0]
+        apmnt_Date = str(event[0])#"June 22, 2020"
+        apmnt_Time = str(event[0])#"4 pm"
+        apmnt_Title = str(event[2])
         return apmnt_Date, apmnt_Time, apmnt_Title
 
     def stop(self):
